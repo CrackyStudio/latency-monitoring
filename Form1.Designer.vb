816ee8eac6b5e8a29ef7a71e6c808777e113bb1e
CrackyStudio <antoine.nivoy@gmail.com>
2019-06-29 14:17:19 +0200

    Initialization

---
+++ Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(0, 0)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Net.NetworkInformation
+
+Public Class Form1
+
+    Protected ReadOnly ping As New System.Net.NetworkInformation.Ping
+    Protected latency As String
+    Protected address As String = "8.8.8.8"
+    Protected niFont As New Font("Tahoma", 16, FontStyle.Regular, GraphicsUnit.Pixel)
+    Protected niBrush As New SolidBrush(Color.White)
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        latency = GetLatency(address)
+        NotifyIcon1.Icon = CreateTextIcon(latency)
+        NotifyIcon1.Text = latency
+    End Sub
+
+    Protected Function GetLatency(ByRef hostNameOrAddress As String)
+        Return ping.Send(hostNameOrAddress).RoundtripTime
+    End Function
+
+    Protected Function CreateTextIcon(ByRef msLatency As String)
+        If msLatency >= 100 Then
+            msLatency = 99
+        End If
+        If msLatency <= 60 Then
+            niBrush = New SolidBrush(Color.Lime)
+        ElseIf msLatency > 60 AndAlso msLatency <= 80 Then
+            niBrush = New SolidBrush(Color.Orange)
+        ElseIf msLatency > 80 AndAlso msLatency < 100 Then
+            niBrush = New SolidBrush(Color.Red)
+        End If
+        Dim bitmapText = New Bitmap(20, 20)
+        Dim g = Graphics.FromImage(bitmapText)
+        Dim hIcon
+        g.Clear(Color.Transparent)
+        g.TextRenderingHint = Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit
+        g.DrawString(msLatency, niFont, niBrush, 0, 0)
+        hIcon = bitmapText.GetHicon()
+        Return Icon.FromHandle(hIcon)
+    End Function
+End Class
